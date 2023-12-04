c20a389c9b0401b56ccc9afd112662b63bb4c47f
Bartosz Oliwa <oliwa.bartosz@gmail.com>
2023-12-04 11:48:03 +0100

    First commit

---
+++ src/api/dto/get-all-flats.dto.ts
@@ -0,0 +1,10 @@
+export class getAllFlatsDto {
+    number: number;
+    market: string;
+    offerId: string | number;
+    offerIdExpected: string | number;
+    offerType: string;
+    offerStatus: string;
+    dateAdded: Date;
+
+}
