bf91d19054a5fe3bc97b2376efe38f8524396ec1
Bartosz Oliwa <oliwa.bartosz@gmail.com>
2024-01-15 15:48:33 +0100

    Refactored houses module

--- src/houses/houses.module.ts
+++ src/houses/houses.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HousesService } from './houses.service';
 import { HousesController } from './houses.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {HousesData} from "./enitities/houses-data.entity";
+import {HousesAnswers} from "./enitities/houses-answers.entity";
+
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([HousesData, HousesAnswers]),
+  ],
   providers: [HousesService],
   controllers: [HousesController]
 })
