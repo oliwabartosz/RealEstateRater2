d2565c186eccbf6a0e94690a7f14532f2c10bf4f
Bartosz Oliwa <oliwa.bartosz@gmail.com>
2024-01-17 11:36:44 +0100

    NestJS starts

--- src/houses/houses.module.ts
+++ src/houses/houses.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { HousesService } from './houses.service';
+import {HousesAnswersService, HousesService} from './houses.service';
 import { HousesController } from './houses.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {HousesData} from "./enitities/houses-data.entity";
@@ -10,7 +10,7 @@ import {HousesAnswers} from "./enitities/houses-answers.entity";
   imports: [
     TypeOrmModule.forFeature([HousesData, HousesAnswers]),
   ],
-  providers: [HousesService],
+  providers: [HousesService, HousesAnswersService],
   controllers: [HousesController]
 })
 export class HousesModule {}
