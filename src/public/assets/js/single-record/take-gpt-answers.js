001d4661ad62f41a7241a093368becec2537d801
Bartosz Oliwa <oliwa.bartosz@gmail.com>
2024-01-31 15:00:11 +0100

    User can click on AI rate to change the radio button

--- src/public/assets/js/single-record/take-gpt-answers.js
+++ src/public/assets/js/single-record/take-gpt-answers.js
@@ -1,4 +1,4 @@
-const replaceWithAIAnswers = () => {
+const replaceAllWithAIAnswers = () => {
     const elements = ['technology', 'legalStatus', 'balcony', 'elevator', 'basement', 'garage', 'garden', 'alarm', 'outbuilding', 'modernization', 'kitchen', 'quality'];
     const answers = {};
 
@@ -9,24 +9,27 @@ const replaceWithAIAnswers = () => {
     }
 }
 
-function checkRadioButton(rate, element) {
-    let radios = document.getElementsByName(element);
+const replaceOneWithAIAnswer = (elementName) => {
+    const rate = getValue(`ai-${elementName}-rate`);
+    checkRadioButton(rate, elementName)
+
+}
+
+
+function checkRadioButton(rate, elementName) {
+    let radios = document.getElementsByName(elementName);
 
     for (const radio of radios) {
         if (radio.value === String(rate)) {
             radio.checked = true;
         }
     }
-
-
 }
-
 
 function getValue(elementName) {
         const nodes = document.getElementsByClassName(elementName);
-        if (!(typeof nodes[0] === "undefined")){
+        if (!(typeof nodes[0] === "undefined")) {
             return Number(nodes[0].innerText.replace("Ocena GPT: ", ""));
         }
-
-
 }
+
